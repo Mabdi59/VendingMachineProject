8193a7a0bc5755bc5e6304bfd5ff4170ea2dedb8
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-02 13:38:31 -0400

    refactor

--- capstone/src/main/java/com/techelevator/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -1,14 +1,31 @@
 package com.techelevator;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMachine {
-	private List<Product> inventory;
+	private List<String> inventory;
 	private double currentBalance;
 
 	public VendingMachine() {
-		inventory = new ArrayList<>();
-		currentBalance = 0.0;
+//		inventory = new ArrayList<>();
+//		currentBalance = 0.0;
+
+		this.inventory = readFile("src/main/resources/inventory.txt");
+		System.out.println(inventory);
+
+	}
+	public List<String> readFile(String path) {
+		List<String> listOfFiles = null;
+		try {
+			listOfFiles = Files.readAllLines(Path.of(path));
+
+		} catch (IOException e){
+			System.out.printf("file not found: %s%n", path);
+		}
+		return listOfFiles;
 	}
 }
