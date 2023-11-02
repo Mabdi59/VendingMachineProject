f6c96ad8e317c373f4aaa40645a96a2389259dfe
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-02 10:48:42 -0400

    new test class: ProductTest

---
+++ capstone/src/test/java/com/techelevator/view/ProductTest.java
@@ -0,0 +1,22 @@
+package com.techelevator.view;
+
+
+import com.techelevator.Product;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ProductTest {
+//    private Product product;
+//    @BeforeEach
+//    public setUp(){
+//
+//    }
+//    @Test
+//    public test_should_add_1_to_quantity(){
+//
+//    }
+//    @Test
+//    public test_should_remove_1_from_quantity(){
+//
+//    }
+}
