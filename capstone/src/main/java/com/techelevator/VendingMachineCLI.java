b84f4f31d33ffb4f0529b1d48abf35c236dd0315
Mohamed Abdi <Moha.abdi105@gmail.com>
2023-11-03 10:38:35 -0400

    Vending Machine

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import com.techelevator.view.Menu;
+import java.util.Scanner;
 
 public class VendingMachineCLI {
 
@@ -27,31 +28,89 @@ public class VendingMachineCLI {
             if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
                 vendingMachine.displayAllItems();
             } else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-                // new account to do transactions
-                CustomerAccount account = new CustomerAccount();
+                CustomerAccount account = vendingMachine.getAccount();
 
-                // ask for money
+                // Ask the user to add money
+                double moneyToAdd = promptForMoney();
+                account.addToBalance(moneyToAdd);
 
-                // ask them to choose an item
+                // Ask the user to select a product
+                String slotLocation = promptForProductSelection(vendingMachine);
+                Product selectedProduct = vendingMachine.searchItemBySlotLocation(slotLocation);
 
-                // charge account
+                if (selectedProduct != null) {
+                    // Check if the user has enough balance to make the purchase
+                    if (account.getBalance() >= selectedProduct.getPrice() && !selectedProduct.isSoldOut()) {
+                        // Charge the account
+                        account.subtractFromBalance(selectedProduct.getPrice());
 
-                // decrease quantity of item
+                        // Decrease the quantity of the product
+                        selectedProduct.decrementQuantity();
 
-                // give them an item
+                        // Give the user the selected product
+                        System.out.println("Here's your " + selectedProduct.getName());
 
-                //give them their change
-
-                // go back to main menu
-
-
-
+                        // Give change if needed
+                        double change = account.giveChange();
+                        System.out.println("Change: $" + change);
+                    } else if (selectedProduct.isSoldOut()) {
+                        System.out.println("SOLD OUT: " + selectedProduct.getName());
+                    } else {
+                        System.out.println("Insufficient balance to purchase this item.");
+                    }
+                }
             } else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
                 break;
             } else if (choice.equals(MAIN_MENU_OPTION_SALES_REPORT)) {
-                // print sales report
+                // Implement sales report functionality
             }
         }
+    }
+
+    private double promptForMoney() {
+        Scanner scanner = new Scanner(System.in);
+        double moneyToAdd = 0.0;
+        while (true) {
+            System.out.print("Enter the amount to add (in dollars): $");
+            if (scanner.hasNextDouble()) {
+                moneyToAdd = scanner.nextDouble();
+                if (moneyToAdd > 0) {
+                    break;
+                } else {
+                    System.out.println("Please enter a valid positive amount.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a valid number.");
+                scanner.next();
+            }
+        }
+        return moneyToAdd;
+    }
+
+    private String promptForProductSelection(VendingMachine vendingMachine) {
+        Scanner scanner = new Scanner(System.in);
+        String slotLocation = "";
+        while (true) {
+            System.out.print("Enter the slot location of the product you want to purchase: ");
+            slotLocation = scanner.nextLine();
+            if (isValidSlotLocation(slotLocation, vendingMachine)) {
+                break;
+            } else {
+                System.out.println("Invalid slot location or product is sold out. Please try again.");
+            }
+        }
+        return slotLocation;
+    }
+
+    private boolean isValidSlotLocation(String slotLocation, VendingMachine vendingMachine) {
+        if (!slotLocation.isEmpty()) {
+            for (Product product : vendingMachine.getInventory()) {
+                if (product.getSlotLocation().equalsIgnoreCase(slotLocation) && !product.isSoldOut()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
