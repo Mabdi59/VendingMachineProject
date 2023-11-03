e0a2874b0f5f0cb5be107f797839992fcb6949bb
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-02 22:20:42 -0400

    refactor

--- capstone/src/main/java/com/techelevator/CustomerAccount.java
+++ capstone/src/main/java/com/techelevator/CustomerAccount.java
@@ -7,13 +7,13 @@ public class CustomerAccount {
         return balance;
     }
 
-    public double deposit(double amount) {
+    public double addToBalance(double amount) {
         if (amount > 0)
             balance += amount;
         return balance;
     }
 
-    public double withdraw(double amount) {
+    public double subtractFromBalance(double amount) {
         if (balance >= amount)
             balance -= amount;
 
@@ -25,7 +25,7 @@ public class CustomerAccount {
             return 0;
 
         double change = getBalance();
-        withdraw(balance);
+        balance = 0.0;
         return change;
     }
 }
--- capstone/src/test/java/com/techelevator/view/CustomerAccountTest.java
+++ capstone/src/test/java/com/techelevator/view/CustomerAccountTest.java
@@ -22,30 +22,30 @@ public class CustomerAccountTest {
     }
 
     @Test
-    public void test_deposit_should_increase_balance_by_5() {
+    public void test_addToBalance_should_increase_balance_by_5() {
         double expected = account.getBalance() + 5.0;
-        double received = account.deposit(5.0);
+        double received = account.addToBalance(5.0);
         assertEquals(expected, received, 0.0);
     }
 
     @Test
-    public void test_withdraw_should_decrease_balance_by_5() {
-        account.deposit(10.0);
+    public void test_subtractFromBalance_should_decrease_balance_by_5() {
+        account.addToBalance(10.0);
         double expected = account.getBalance() - 5.0;
-        double received = account.withdraw(5.0);
+        double received = account.subtractFromBalance(5.0);
         assertEquals(expected, received, 0.0);
     }
 
     @Test
     public void test_should_give_correct_change() {
-        account.deposit(2.5);
+        account.addToBalance(2.5);
         double expected = 2.5;
         double received = account.giveChange();
         assertEquals(expected, received, 0.0);
     }
     @Test
     public void test_should_have_zero_balance_after_give_change() {
-        account.deposit(2.5);
+        account.addToBalance(2.5);
         account.giveChange();
         double expected = 0;
         double received = account.getBalance();
