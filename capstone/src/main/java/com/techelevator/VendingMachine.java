bbce71730d9047b78d15f94902e61d2d68d647c4
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-03 04:15:31 -0400

    added search and display features

--- capstone/src/main/java/com/techelevator/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 public class VendingMachine {
     private Set<Product> inventory;
+    private CustomerAccount account;
 
     public Set<Product> getInventory() {
         return inventory;
@@ -13,13 +14,39 @@ public class VendingMachine {
         this.inventory = new Inventory(filePath).getInventory();
     }
 
-    public void displayItems() {
+    public void createAccount() {
+        this.account = new CustomerAccount();
+    }
+
+    public CustomerAccount getAccount() {
+        return account;
+    }
+
+    public void displayItem(Product product) {
+        System.out.printf("%s %s %s %s%n",
+                product.getSlotLocation(),
+                product.getName(),
+                product.getPrice(),
+                product.getType());
+    }
+
+    public void displayAllItems() {
         for (Product product : inventory) {
-            System.out.printf("%s %s %s %s%n",
-                    product.getSlotLocation(),
-                    product.getName(),
-                    product.getPrice(),
-                    product.getType());
+            displayItem(product);
+        }
+    }
+
+    public Product searchItemBySlotLocation(String slotLocation) {
+        for (Product product : inventory) {
+            if (product.isSoldOut()) {
+                System.out.println("SOLD OUT: " + product.getName());
+                return null;
+            }
+
+            if (product.getSlotLocation() == slotLocation) {
+                System.out.printf("You chose: " + product.getName());
+                return product;
+            }
         }
     }
 }
