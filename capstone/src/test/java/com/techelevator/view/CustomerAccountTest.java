9a474c51a9ae495484b2078666976e1945000e11
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-03 03:42:43 -0400

    new test

--- capstone/src/test/java/com/techelevator/view/CustomerAccountTest.java
+++ capstone/src/test/java/com/techelevator/view/CustomerAccountTest.java
@@ -1,55 +1,55 @@
 package com.techelevator.view;
 
-import com.techelevator.VendingMachine;
+import com.techelevator.CustomerAccount;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertEquals;
 
-public class VendingMachineTest {
-    private VendingMachine vendingMachine;
+public class CustomerAccountTest {
+    private CustomerAccount account;
 
     @BeforeEach
     public void setUp() {
-        vendingMachine = new VendingMachine();
+        account = new CustomerAccount();
     }
 
     @Test
     void test_should_start_with_balance_of_zero() {
         double expected = 0.0;
-        double received = vendingMachine.getBalance();
+        double received = account.getBalance();
         assertEquals(expected, received, 0.0);
     }
 
     @Test
     void test_addToBalance_should_increase_balance_by_5() {
-        double expected = vendingMachine.getBalance() + 5.0;
-        double received = vendingMachine.addToBalance(5.0);
+        double expected = account.getBalance() + 5.0;
+        double received = account.addToBalance(5.0);
         assertEquals(expected, received, 0.0);
     }
 
     @Test
     void test_subtractFromBalance_should_decrease_balance_by_5() {
-        vendingMachine.addToBalance(10.0);
-        double expected = vendingMachine.getBalance() - 5.0;
-        double received = vendingMachine.subtractFromBalance(5.0);
+        account.addToBalance(10.0);
+        double expected = account.getBalance() - 5.0;
+        double received = account.subtractFromBalance(5.0);
         assertEquals(expected, received, 0.0);
     }
 
     @Test
     void test_should_give_correct_change() {
-        vendingMachine.addToBalance(2.5);
+        account.addToBalance(2.5);
         double expected = 2.5;
-        double received = vendingMachine.giveChange();
+        double received = account.giveChange();
         assertEquals(expected, received, 0.0);
     }
 
     @Test
     void test_should_have_zero_balance_after_give_change() {
-        vendingMachine.addToBalance(2.5);
-        vendingMachine.giveChange();
+        account.addToBalance(2.5);
+        account.giveChange();
         double expected = 0;
-        double received = vendingMachine.getBalance();
+        double received = account.getBalance();
         assertEquals(expected, received, 0.0);
     }
 }
