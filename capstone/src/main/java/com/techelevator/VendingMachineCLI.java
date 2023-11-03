3dbc0618dbeea87723b7dc4fde47ad66fd34a376
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-03 04:16:32 -0400

    added secret menu and init new transaction

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -7,7 +7,9 @@ public class VendingMachineCLI {
     private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
     private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
     private static final String MAIN_MENU_OPTION_EXIT = "Exit";
-    private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT};
+    private static final String MAIN_MENU_OPTION_SALES_REPORT = "Sales Report";
+
+    private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_OPTION_SALES_REPORT};
 
     private Menu menu;
 
@@ -18,15 +20,27 @@ public class VendingMachineCLI {
     public void run() {
         VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.loadInventory("src/main/resources/inventory.txt");
+        vendingMachine.createAccount();
         while (true) {
             String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
             if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-                vendingMachine.displayItems();
+                vendingMachine.displayAllItems();
             } else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+                // new account to do transactions
+                CustomerAccount account = new CustomerAccount();
+
+                // ask them what they want
+
+
                 // do purchase
+
+
+
             } else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
                 break;
+            } else if (choice.equals(MAIN_MENU_OPTION_SALES_REPORT)) {
+                // print sales report
             }
         }
     }
