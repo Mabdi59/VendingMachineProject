4a61d389430b4ee1727dc9d30678d30f8eae6e6f
Dominic.Lobban <dominic.lobban@gmail.com>
2023-11-04 13:17:39 -0400

    dedicated record keeping

---
+++ capstone/src/main/java/com/techelevator/History.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class History {
+    private List<String> logs = new ArrayList<>();
+    private Map<String, Integer> salesReport = new TreeMap<>();
+
+//    private void init(){
+//
+//    }
+//    private class SalesReport{
+//
+//    }
+//    private class Logs{
+//
+//    }
+}
